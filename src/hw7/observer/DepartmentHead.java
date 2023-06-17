e4fe21ebd3cac1892527419b7b02028b41db3a54
SergSemak <semakin.s.v@gmail.com>
2023-06-18 01:29:04 +0300

    fixed hw7

--- src/hw7/observer/DepartmentHead.java
+++ src/hw7/observer/DepartmentHead.java
@@ -2,17 +2,7 @@ package hw7.observer;
 
 import java.util.Random;
 
-public class DepartmentHead implements Observer{
-
-    private static Random random = new Random();
-
-    private String name;
-
-    private String nameVacancy;
-
-    private double minSalary;
-
-    private boolean hasJob;
+public class DepartmentHead extends Worker{
 
     public DepartmentHead(String name, String vacancy) {
         this.name = name;
@@ -21,27 +11,8 @@ public class DepartmentHead implements Observer{
         hasJob = false;
     }
 
-
     @Override
-    public void receiveOffer(String companyName, double salary, String vacancy) {
-        if (minSalary <= salary){
-            System.out.printf("Руководитель %s (%.2f) >>> Мне нужна эта работа (%s)! [%s - %.2f]\n",
-                    name, minSalary, vacancy, companyName, salary);
-            hasJob = true;
-        }
-        else {
-            System.out.printf("Руководитель %s >>> Я найду работу (%s) получше (%.2f)! [%s - %.2f]\n",
-                    name, vacancy, minSalary, companyName, salary);
-        }
-    }
-
-    @Override
-    public boolean takeJob(String vacancy) {
-        return hasJob;
-    }
-
-    @Override
-    public String getVacancyName() {
-        return nameVacancy;
+    public String toString() {
+        return "Руководитель";
     }
 }
--- src/hw7/observer/Master.java
+++ src/hw7/observer/Master.java
@@ -2,17 +2,7 @@ package hw7.observer;
 
 import java.util.Random;
 
-public class Master implements Observer {
-
-    private static Random random = new Random();
-
-    private String name;
-
-    private String nameVacancy;
-
-    private double minSalary;
-
-    private boolean hasJob;
+public class Master extends Worker {
 
     public Master(String name, String vacancy) {
         this.name = name;
@@ -22,25 +12,7 @@ public class Master implements Observer {
     }
 
     @Override
-    public void receiveOffer(String companyName, double salary, String vacancy) {
-        if (minSalary <= salary){
-            System.out.printf("Рабочий %s (%.2f) >>> Мне нужна эта работа (%s)! [%s - %.2f]\n",
-                    name, minSalary, vacancy, companyName, salary);
-            hasJob = true;
-        }
-        else {
-            System.out.printf("Рабочий %s >>> Я найду работу (%s) получше (%.2f)! [%s - %.2f]\n",
-                    name, vacancy, minSalary, companyName, salary);
-        }
-    }
-
-    @Override
-    public boolean takeJob(String vacancy) {
-        return hasJob;
-    }
-
-    @Override
-    public String getVacancyName() {
-        return nameVacancy;
+    public String toString() {
+        return "Мастер";
     }
 }
